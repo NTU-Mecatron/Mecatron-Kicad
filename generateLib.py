54dc969d847f317f4fdd02e3451c52e9eb090af8
Jerry Zhang <69628481+jerryrzhang@users.noreply.github.com>
2026-09-14 23:02:12 +0800

    feat: changed endpoints to github host

--- generateLib.py
+++ generateLib.py
@@ -48,8 +48,8 @@ meta = json.load(open("metadata.json"))
 
 version = meta["versions"][0]["version"] #grab versions out 
 meta["versions"][0].update({ # inject post package fields into the metadata. Defined here: https://dev-docs.kicad.org/en/addons/index.html
-    #"download_url": f"https://github.com/NTU-Mecatron/Mecatron-Kicad/releases/download/v{version}/{zipPath.name}",
-    "download_url": f"http://localhost:8000/dist/{zipPath.name}",
+    "download_url": f"https://github.com/NTU-Mecatron/Mecatron-Kicad/releases/download/v{version}/{zipPath.name}",
+    #"download_url": f"http://localhost:8000/dist/{zipPath.name}",
     "download_sha256": zipSha,
     "download_size": zipSize,
     "install_size": installSize,
@@ -68,8 +68,8 @@ repository =  {
     "name": meta["name"],
     "maintainer": meta["maintainer"],
     "packages": {
-        "url": "http://localhost:8000/pcm/packages.json",
-        #"url": "https://raw.githubusercontent.com/NTU-Mecatron/Mecatron-Kicad/main/pcm/packages.json",
+        #"url": "http://localhost:8000/pcm/packages.json",
+        "url": "https://raw.githubusercontent.com/NTU-Mecatron/Mecatron-Kicad/main/pcm/packages.json",
         "sha256": sha256(pkgPath),
         "update_timestamp": int(time.time()),
     },    
